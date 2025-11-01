59834e0aaf578cb62f97b84b1effa00ff32c955b
Dione Nascimento <90805395+PortalDosDados@users.noreply.github.com>
2025-11-01 16:53:37 -0300

    Sumarry

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ import streamlit as st
 # Foto no topo
 st.image("../streamlit_portal_dos_dados/assets/minha_foto.jpg", width=160)
 
+# Configurações da página
+st.set_page_config(
+    page_title="Portal dos Dados",       # Título da aba
+    page_icon="assets/portal.png",   # Ícone da aba (pode ser .ico, .png ou emoji)
+    layout="wide"                        # Layout da página (opcional)
+)
+
 # Texto de introdução
 st.markdown('''
 Olá! Sou **Dione Nascimento**, um profissional com **15 anos de experiência em Manutenção Industrial**. 
