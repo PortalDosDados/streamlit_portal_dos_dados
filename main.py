90a93a7fef973609e6e0b767c784fb4bdc4bba9f
Dione Nascimento <90805395+PortalDosDados@users.noreply.github.com>
2025-11-02 13:03:25 -0300

    sumari

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import streamlit as st
+from streamlit import navigation
+
 
 # Foto no topo
-st.image("../streamlit_portal_dos_dados/assets/minha_foto.jpg", width=160)
+st.image("../streamlit_portal_dos_dados/assets/minha_foto.png", width=160)
 
 # Configurações da página
 st.set_page_config(
     page_title="Portal dos Dados",       # Título da aba
     page_icon="assets/portal.png",   # Ícone da aba (pode ser .ico, .png ou emoji)
     layout="wide"                        # Layout da página (opcional)
+)
+
+# Fundo da página (CSS)
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-image: url('https://www.example.com/seu_fundo.png');
+        background-size: cover;   /* cobre toda a tela */
+        background-attachment: fixed;  /* mantém fixo ao rolar */
+        background-position: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
 )
 
 # Texto de introdução
