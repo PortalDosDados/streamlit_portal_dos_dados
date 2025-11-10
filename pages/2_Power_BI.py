03c7a00955db459c6aa22f7ecec5ee2210f5a4cb
Dione Nascimento <90805395+PortalDosDados@users.noreply.github.com>
2025-11-09 22:51:59 -0300

    Update 2_Power_BI.py

--- pages/2_Power_BI.py
+++ pages/2_Power_BI.py
@@ -79,6 +79,9 @@ permitindo uma tomada de decisão rápida e baseada em dados para otimizar custo
 a eficiência operacional.
 ''')
 
+powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=b40c8b5e-e562-4591-b920-61884a06ae3e&autoAuth=true&ctid=da49a844-e2e3-40af-86a6-c3819d704f49"
+st.components.v1.iframe(powerbi_url, height=900, scrolling=True)
+'''
 # Criando três colunas para as imagens
 col1, col2, col3 = st.columns(3)
 
@@ -91,7 +94,7 @@ with col2:
 with col3:
     st.image('./assets/log_03.png', width=700)
 
-
+'''
 st.subheader('Principais Análises e Recursos do Painel')
 
 st.markdown('''
