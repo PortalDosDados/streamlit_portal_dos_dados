184c5ad645907dfa29cf050ee1fe4d02aaf400ea
Dione Nascimento <90805395+PortalDosDados@users.noreply.github.com>
2025-11-20 09:48:43 -0300

    update css

--- Dione_Nascimento.py
+++ Dione_Nascimento.py
@@ -1,5 +1,12 @@
 import streamlit as st
 
+def load_css(file_path: str):
+    with open(file_path, "r", encoding="utf-8") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css("style.css")
+
+
 # Configurações da página
 st.set_page_config(
     page_title='Dione Nascimento',
@@ -56,25 +63,14 @@ with col2:
 ''', unsafe_allow_html=True)
 
 
-# CTA mais destacado
+# Botão do Link
 st.markdown("""
 <div style="margin-top: 20px; margin-bottom: 25px;">
     <a href='https://www.linkedin.com/in/dione-nascimento-37287a233' target='_blank'>
-        <button style="
-            background-color: #0A66C2;
-            color: white;
-            padding: 12px 22px;
-            border: none;
-            border-radius: 6px;
-            font-size: 17px;
-            cursor: pointer;
-        ">
-        Acessar meu LinkedIn
-        </button>
+        <button class="btn-portal">Acessar meu LinkedIn</button>
     </a>
 </div>
 """, unsafe_allow_html=True)
-
 
 # Banner com largura total alinhada ao conteúdo principal
 st.image('./assets/fundo.jpg', use_container_width=True)
