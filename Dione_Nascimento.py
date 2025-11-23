18c3c4eda390c85195f3ece47c171438b52bef07
Dione Nascimento <morrone.dione@gmail.com>
2025-11-23 10:47:27 -0300

    Add LinkedIn button to social section
    Introduced a LinkedIn button alongside existing YouTube and GitHub buttons in the social section, reorganizing the layout to include three columns for better presentation and user connectivity options.

--- Dione_Nascimento.py
+++ Dione_Nascimento.py
@@ -63,11 +63,27 @@ Conteúdo focado em aplicação prática, aprendizado direto e geração de resu
 """)
 
 
-# Seção de Botões (YouTube e GitHub lado a lado)
-col_btn1, col_btn2, col_vazia = st.columns([1, 1, 3])
+# Seção de Botões de Redes Sociais
+st.markdown("### Conecte-se comigo:")
+
+# Cria 3 colunas para os botões e uma coluna vazia para espaçamento
+col_btn1, col_btn2, col_btn3, col_vazia = st.columns([1, 1, 1, 2])
 
 with col_btn1:
-    # Botão do YouTube (Seu código original, mantido)
+    # Botão do LinkedIn (NOVO)
+    st.markdown("""
+    <a href='https://www.linkedin.com/in/dione-nascimento-37287a233/' target='_blank'>
+        <button class="btn-linkedin">
+            <svg viewBox="0 0 24 24" width="24" height="24">
+                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" fill="white"/>
+            </svg>
+            <span>LinkedIn</span>
+        </button>
+    </a>
+    """, unsafe_allow_html=True)
+
+with col_btn2:
+    # Botão do YouTube
     st.markdown("""
     <a href='https://www.youtube.com/@Portal_dos_Dados' target='_blank'>
         <button class="btn-youtube">
@@ -82,8 +98,8 @@ with col_btn1:
     </a>
     """, unsafe_allow_html=True)
 
-with col_btn2:
-    # Botão do GitHub (Adicionado)
+with col_btn3:
+    # Botão do GitHub
     st.markdown("""
     <a href='https://github.com/PortalDosDados' target='_blank'>
         <button class="btn-github">
