8727b7811b5c7438502928decbf1705be1beab8f
Dione Nascimento <morrone.dione@gmail.com>
2025-11-23 10:43:35 -0300

    Add GitHub button and unify social button styles
    Introduced a GitHub button alongside the existing YouTube button, displaying them side by side using Streamlit columns. Refactored CSS to unify the style of social buttons (LinkedIn, YouTube, GitHub), reducing code repetition and improving maintainability.

--- Dione_Nascimento.py
+++ Dione_Nascimento.py
@@ -62,17 +62,35 @@ st.markdown("""
 Conteúdo focado em aplicação prática, aprendizado direto e geração de resultados reais.
 """)
 
-# Botão do Canal
-st.markdown("""
-<a href='https://www.youtube.com/@Portal_dos_Dados' target='_blank'>
-<button class="btn-youtube">
-<svg viewBox="0 0 24 24">
-<path d="M23.5 6.2s-.2-1.7-.8-2.4c-.8-.9-1.7-.9-2.1-1C17.4 2.5 12 2.5 12 2.5h-.1s-5.4 0-8.6.3c-.4.1-1.3.1-2.1 1C1.5 
-4.5 1.3 6.2 1.3 6.2S1 8.3 1 10.5v1.9c0 2.2.3 4.3.3 4.3s.2 1.7.8 2.4c.8.9 1.9.9 2.4 
-1C7 20 12 20 12 20s5.4 0 8.6-.3c.4-.1 1.3-.1 2.1-1 .6-.7.8-2.4.8-2.4s.3-2.1.3-4.3v-1.9c0-2.2-.3-4.3-.3-4.3zM9.8 15.3V8.7l6.4 
-3.3-6.4 3.3z"/>
-</svg>
-Conheça meu Canal
-</button>
-</a>
-""", unsafe_allow_html=True)
+
+# Seção de Botões (YouTube e GitHub lado a lado)
+col_btn1, col_btn2, col_vazia = st.columns([1, 1, 3])
+
+with col_btn1:
+    # Botão do YouTube (Seu código original, mantido)
+    st.markdown("""
+    <a href='https://www.youtube.com/@Portal_dos_Dados' target='_blank'>
+        <button class="btn-youtube">
+            <svg viewBox="0 0 24 24" width="24" height="24">
+                <path d="M23.5 6.2s-.2-1.7-.8-2.4c-.8-.9-1.7-.9-2.1-1C17.4 2.5 12 2.5 12 2.5h-.1s-5.4 0-8.6.3c-.4.1-1.3.1-2.1 1C1.5 
+                4.5 1.3 6.2 1.3 6.2S1 8.3 1 10.5v1.9c0 2.2.3 4.3.3 4.3s.2 1.7.8 2.4c.8.9 1.9.9 2.4 
+                1C7 20 12 20 12 20s5.4 0 8.6-.3c.4-.1 1.3-.1 2.1-1 .6-.7.8-2.4.8-2.4s.3-2.1.3-4.3v-1.9c0-2.2-.3-4.3-.3-4.3zM9.8 15.3V8.7l6.4 
+                3.3-6.4 3.3z" fill="white"/>
+            </svg>
+            <span>YouTube</span>
+        </button>
+    </a>
+    """, unsafe_allow_html=True)
+
+with col_btn2:
+    # Botão do GitHub (Adicionado)
+    st.markdown("""
+    <a href='https://github.com/PortalDosDados' target='_blank'>
+        <button class="btn-github">
+            <svg viewBox="0 0 24 24" width="24" height="24">
+                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" fill="white"/>
+            </svg>
+            <span>GitHub</span>
+        </button>
+    </a>
+    """, unsafe_allow_html=True)
